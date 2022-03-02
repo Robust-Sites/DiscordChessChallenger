c2b32e51ab275d455d9ca84c799ec80490caacb7
jjmiranda8 <jmlivingfree@gmail.com>
2022-03-02 17:42:53 -0600

    push for restart actually

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import chess_board_render as r
 
 # Validation for piece quantities
-active_pieces = {
-    "QQ": ['A1', 'B2', 'H7'], #Queen
-    "RR": ['C3'],
-    "BB": ['C4'],
-    "KN": ['F2', 'G7'], #King
-    "PP": ['C7'],
-    "KI": ['H1']
-}
+# active_pieces = {
+#     "QQ": ['A1', 'B2', 'H7'], #Queen
+#     "RR": ['C3'],
+#     "BB": ['C4'],
+#     "KN": ['F2', 'G7'], #King
+#     "PP": ['C7'],
+#     "KI": ['H1']
+# }
 
 
 def main():
-    print(r.max_piece_count(active_pieces))
+    pass
 
 
 if __name__ == "__main__":
