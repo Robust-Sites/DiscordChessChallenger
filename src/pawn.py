663839b9d49fcbd35efda66d9bacd39b3640a236
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-06 18:53:50 -0500

    add pawn class that inherits from piece

---
+++ src/pawn.py
@@ -0,0 +1,8 @@
+class Pawn(Piece):
+    def __init__(self, coordinate, color)
+        super().__init__(coordinate, color)
+
+    def possible_moves():
+        # this may return an array of all possible target
+        # coordinates
+        pass
