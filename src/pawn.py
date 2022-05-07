3af66c1541f5aaa88aee882309244df175c89a05
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-06 19:11:37 -0500

    add if __name__ == __main__ and n syntax error in pawn

--- src/pawn.py
+++ src/pawn.py
@@ -1,8 +1,13 @@
+from piece import Piece 
+
 class Pawn(Piece):
-    def __init__(self, coordinate, color)
+    def __init__(self, coordinate, color):
         super().__init__(coordinate, color)
 
     def possible_moves():
         # this may return an array of all possible target
         # coordinates
         pass
+
+if __name__ == "__main__":
+    main()
