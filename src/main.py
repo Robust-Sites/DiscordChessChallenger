71a58ab1b39b1c34c87120349e6f1ffef33d776e
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-06 19:19:00 -0500

    remove comments and starting_board object

--- src/main.py
+++ src/main.py
@@ -1,20 +1,7 @@
 import chess_board_render as r
 
-# Validation for piece quantities
-# active_pieces = {
-#     "QQ": ['A1', 'B2', 'H7'], #Queen
-#     "RR": ['C3'],
-#     "BB": ['C4'],
-#     "KN": ['F2', 'G7'], #King
-#     "PP": ['C7'],
-#     "KI": ['H1']
-# }
-starting_board = {
-    'A1': 'KI',
-    'B1': 'KI'}
-
 def main():
-    print(r.render_board(starting_board))
+    print(r.render_board())
     pass 
 
 
