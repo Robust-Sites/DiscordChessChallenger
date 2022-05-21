72abc8852c640ca95088b3f579a778309bd66bf9
Manny-Ventura <wow.mannyv@gmail.com>
2022-05-20 21:36:14 -0400

    Created first draft of bishop function possible_moves(). a bit long, but clear to follow I think.

---
+++ src/bishop.py
@@ -0,0 +1,47 @@
+from piece import Piece
+
+class Bishop(Piece):
+    def __init__(self, coordinate, color):
+        super().__init__(coordinate, color)
+
+    def possible_moves(self):
+        possible_moves = []
+        y_equals_x_set = self.y_equals_x_moves()
+        possible_moves.append(y_equals_x_set)
+        y_equals_negative_x_set = self.y_equals_negative_x_moves()
+        possible_moves.append(y_equals_negative_x_set)
+        return possible_moves
+        
+
+    def y_equals_x_moves(self):
+        allowed_moves = []
+        min_value = 0
+        max_value = 7
+        coordinate_copy = [self.coordinate[0], self.coordinate[1]]
+        while coordinate_copy[0] > min_value or coordinate_copy[1] > min_value:
+            coordinate_copy[0] -= 1
+            coordinate_copy[1] -= 1
+        while coordinate_copy[0] <= max_value or coordinate_copy[1] <= max_value:
+            if coordinate_copy[0] != self.coordinate[0] and coordinate_copy[1] != self.coordinate[1]:
+                allowed_moves.append((coordinate_copy[0], coordinate_copy[1]))
+            coordinate_copy[0] += 1
+            coordinate_copy[1] += 1
+        return allowed_moves
+
+    def y_equals_negative_x_moves(self):
+        allowed_moves = []
+        min_value = 0
+        max_value = 7
+        coordinate_copy = [self.coordinate[0], self.coordinate[1]]
+        while coordinate_copy[0] < max_value or coordinate_copy[1] < max_value:
+            coordinate_copy[0] += 1
+            coordinate_copy[1] += 1
+        while coordinate_copy[0] >= min_value or coordinate_copy[1] >= min_value:
+            if coordinate_copy[0] != self.coordinate[0] and coordinate_copy[1] != self.coordinate[1]:
+                allowed_moves.append((coordinate_copy[0], coordinate_copy[1]))
+            coordinate_copy[0] -= 1
+            coordinate_copy[1] -= 1
+
+if __name__ == "__main__":
+    main()
+
--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import chess_board_render as r
 import pawn as p
 import knight as n
 import rook as r
+import bishop as b
 
 def main():
    
-    rook = r.Rook('55', 'black')
-    print(rook.possible_moves())
+    bishop = b.Bishop((4,4), 'Black')
+    print(bishop.possible_moves())
 
 
 if __name__ == "__main__":
