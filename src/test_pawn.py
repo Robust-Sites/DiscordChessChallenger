33449c26fa682778bd64a1e20ddc364be1c8b9c1
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-24 19:18:29 -0500

    refactor the rest of the assertions in test pawn

--- src/test_pawn.py
+++ src/test_pawn.py
@@ -5,7 +5,6 @@ def assert_tuples(piece, array_of_tuples):
     for tuple in array_of_tuples:
         assert tuple in piece.possible_moves()
 
-
 def test_white_center_possible_moves():
     test_pawn = Pawn((3, 3), "white")
     assert_tuples(test_pawn, [(4, 4), (3, 4), (2, 4)])
@@ -13,31 +12,23 @@ def test_white_center_possible_moves():
 
 def test_white_left_border_first_turn_possible_moves():
     test_pawn = Pawn((0, 1), "white")
-    assert (0,3) in test_pawn.possible_moves()
-    assert (0,2) in test_pawn.possible_moves()
-    assert (1,2) in test_pawn.possible_moves()
-
+    assert_tuples(test_pawn, [(0, 3), (0, 2), (1, 2)])
 
 def test_white_right_border_first_turn_possible_moves():
     test_pawn = Pawn((7, 1), "white")
-    assert (6,2) in test_pawn.possible_moves()
-    assert (7,2) in test_pawn.possible_moves()
-    assert (7,3) in test_pawn.possible_moves()
+    assert_tuples(test_pawn, [(6, 2), (7, 2), (7, 3)])
 
 def test_black_right_border_first_turn_possible_moves():
     test_pawn = Pawn((0,6), "black")
-    assert (0,4) in test_pawn.possible_moves()
-    assert (0,5) in test_pawn.possible_moves()
-    assert (1,5) in test_pawn.possible_moves()
+    assert_tuples(test_pawn, [(0, 4), (0, 5), (1, 5)])
 
 def test_black_left_border_first_turn_possible_moves():
     test_pawn = Pawn((7,6), "black")
-    assert (7,4) in test_pawn.possible_moves()
-    assert (7,5) in test_pawn.possible_moves()
-    assert (6,5) in test_pawn.possible_moves()
+    assert_tuples(test_pawn, [(7, 4), (7, 5), (6, 5)])
 
 def test_black_center_possible_moves():
     test_pawn = Pawn((2,5), "black")
+    assert_tuples(test_pawn, [(1, 4), (2, 4), (3, 4)])
     assert (1,4) in test_pawn.possible_moves()
     assert (2,4) in test_pawn.possible_moves()
     assert (3,4) in test_pawn.possible_moves()
