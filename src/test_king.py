a66f43b5f47f0ac3a6a5c3ef1748c06dcedf4a8c
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-25 00:46:48 -0500

    add comments to the pytest_helpers functions, add and refactor test_king with tuples_from_x_and_y_lists helper from pytest_helpers

--- src/test_king.py
+++ src/test_king.py
@@ -5,17 +5,20 @@ from king import King
 
 def test_possible_moves():
     test_king = King((3, 3), "white")
-    assert_array_of_tuples(test_king, [(4, 4),(4, 3),(4, 2),(3, 4),(3, 2),(2, 4),(2, 3),(2, 2)])
+    assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(8, [4, 4, 4, 3, 3, 2, 2, 2], [4, 3, 2, 4, 2, 4, 3, 2]))
 
 def test_left_border_possible_moves():
     test_king = King((0, 1), "black")
     assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(5, [0, 0, 1, 1, 1], [2, 0, 2, 1, 0]))
 
 def test_right_border_possible_moves():
-    pass
+    test_king = King((5, 1), "black")
+    assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(5, [4, 4, 4, 5, 5], [2, 1, 0, 2, 0]))
 
 def test_bottom_border_possible_moves():
-    pass
+    test_king = King((2, 0), "black")
+    assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(5, [1, 1, 2, 3, 3], [1, 0, 1, 1, 0]))
 
-def test_top_border_possible_moves():
-    pass
+def test_top_right_corner_possible_moves():
+    test_king = King((7, 7), "black")
+    assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(3, [6, 6, 7], [7, 6, 6]))
