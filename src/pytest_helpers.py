1b18f06179f3689a9ea7665336ba96d00cdb1c25
jjmiranda8 <jmlivingfree@gmail.com>
2022-05-25 00:16:44 -0500

    add logic to tuples_from_x_and_y_lists in pytest_helpers, add test to test_king

--- src/pytest_helpers.py
+++ src/pytest_helpers.py
@@ -2,5 +2,11 @@ def assert_array_of_tuples(piece, array_of_tuples):
     for tuple in array_of_tuples:
         assert tuple in piece.possible_moves()
 
-def tuples_from_x_and_y_lists():
-    pass
+def tuples_from_x_and_y_lists(number_of_tuples, list_of_x_values, list_of_y_values):
+    tuples = []
+    count = 0
+    while count < number_of_tuples:
+        tuples.append((list_of_x_values[count], list_of_y_values[count])) 
+        count += 1
+    return tuples
+
--- src/test_king.py
+++ src/test_king.py
@@ -1,5 +1,6 @@
 import pytest
 from pytest_helpers import assert_array_of_tuples
+from pytest_helpers import tuples_from_x_and_y_lists 
 from king import King
 
 def test_possible_moves():
@@ -7,7 +8,8 @@ def test_possible_moves():
     assert_array_of_tuples(test_king, [(4, 4),(4, 3),(4, 2),(3, 4),(3, 2),(2, 4),(2, 3),(2, 2)])
 
 def test_left_border_possible_moves():
-    pass
+    test_king = King((0, 1), "black")
+    assert_array_of_tuples(test_king, tuples_from_x_and_y_lists(5, [0, 0, 1, 1, 1], [2, 0, 2, 1, 0]))
 
 def test_right_border_possible_moves():
     pass
