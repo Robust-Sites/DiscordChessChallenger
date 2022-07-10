aa27523de534dd3033c52ad6f6b6f9932eb0bd5d
Manny-Ventura <wow.mannyv@gmail.com>
2022-07-10 13:42:18 -0400

    Created test up and right moves

--- src/test_rook.py
+++ src/test_rook.py
@@ -34,3 +34,12 @@ def test_down_possible_moves():
     for number in array_of_column_values:
         test_values.append((2,number))
     assert_array_of_tuples(test_rook, test_values)
+
+def test_up_and_right_possible_moves():
+    test_rook = Rook((0,0), "white")
+    test_values = []
+    array_of_values = return_array_of_numbers_except(0)
+    for number in array_of_values:
+        test_values.append((0,number))
+        test_values.append((number,0))
+    assert_array_of_tuples(test_rook, test_values)
